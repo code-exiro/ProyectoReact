e92ebd6a1ec97f0b4935c1cc103413a64352b103
erizo <nandezemir@gmail.com>
2024-05-16 17:12:36 -0600

    3.1 Enrutador inicio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PokemonViewer from './PokemonViewer';
 import SavedPokemons from './SavedPokemons';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pokemon Viewer</h1>
-      <PokemonViewer />
-      <SavedPokemons />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/saved">Saved Pokemons</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<PokemonViewer />} />
+          <Route path="/saved" element={<SavedPokemons />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
